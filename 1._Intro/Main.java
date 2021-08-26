21bbc89a82a2f632828feade5ee2c451fd6c0b39
Anders Latif <andl@kea.dk>
2021-08-26 11:44:19 +0200

    Created the Intro folder

---
+++ 1._Intro/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Main {
+
+
+    // generics
+/*    public static <T> void out(T content) {
+        System.out.println(content);
+    }*/
+
+    public static void main(String[] args) {
+
+        // I want Systemos.out.println instead. Solve this.
+
+
+        Systemos.out.println("Say hello");
+        Systemos.out.println(5);
+        Systemos.out.println(3.50);
+        // support all primitive data types you can think of
+
+    }
+
+}
---
+++ 1._Intro/Systemos.java
@@ -0,0 +1,19 @@
+public class Systemos {
+
+    public static class out {
+
+        public static void println(String content) {
+            System.out.println(content);
+        }
+
+        public static void println(int content) {
+            System.out.println(content);
+        }
+
+        public static void println(double content) {
+            System.out.println(content);
+        }
+
+    }
+
+}
