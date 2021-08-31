8729b47345013c302d324b21fcee3bfcdb288d6a
Anders Latif <andl@kea.dk>
2021-08-31 11:45:04 +0200

    Implemented activity on Octopus

--- 2._OOP_Recap/Main.java
+++ 2._OOP_Recap/Main.java
@@ -2,7 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
         Octopus otto = new Octopus();
-        System.out.println(otto.move());
+        System.out.println(otto.activity(1));
+        System.out.println(otto.activity(2));
+        System.out.println(otto.activity(3));
+        System.out.println(otto.activity(4));
+        System.out.println(otto.activity(5));
+        System.out.println(otto.activity(6));
+        System.out.println(otto.activity(7));
 
 
     }
--- 2._OOP_Recap/Octopus.java
+++ 2._OOP_Recap/Octopus.java
@@ -1,5 +1,29 @@
 public class Octopus extends Animal {
 
-    // implement a custom move method that returns the String "Swish Swosh"
+    public String activity() {
+        String activity;
+
+        // todo make activity number a random number from 1 - 10
+        int activityNumber = 1;
+
+        switch (activityNumber) {
+            case 1: activity = "Eat";
+                    break;
+            case 2: activity = "Mate";
+                    break;
+            case 3: activity = "Sleep";
+                    break;
+            case 4, 5: activity = "Jet Ink";
+                    break;
+            default: activity = move();
+        }
+
+        return activity;
+    }
+
+    @Override
+    public String move() {
+        return "Swish Swosh";
+    }
 
 }
