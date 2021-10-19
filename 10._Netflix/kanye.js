7df954cb55cecc1aa3ac7c0df010ec560f3b129d
Anders Latif <andl@kea.dk>
2021-10-19 11:14:22 +0200

    Finished the Kanye.Rest project

--- 10._Netflix/kanye.js
+++ 10._Netflix/kanye.js
@@ -1,8 +1,25 @@
-fetch("https://api.kanye.rest/")
-.then(response => response.json())
-.then(result => {
-    console.log(result.quote)
-});
+function getKanyeQuote() {
+    fetch("https://api.kanye.rest/")
+        .then(response => response.json())
+        .then(result => {
+            const quoteHeader = document.getElementById("kanye-quote");
+            quoteHeader.innerText = result.quote;
+        });
+}
+getKanyeQuote();
 
-//todo put the quote on the website
-// you are allowed to change the HTML as well
+
+document.getElementById("btn-laugh").addEventListener("click", () => addQuoteToList("laugh"));
+document.getElementById("btn-wtf").addEventListener("click", () => addQuoteToList("wtf"));
+document.getElementById("btn-horny").addEventListener("click", () => addQuoteToList("horny"));
+
+// todo only allow the quote to be added once
+
+function addQuoteToList(listName) {
+    const quote = document.getElementById("kanye-quote").innerText;
+    const paragraph = document.createElement("p");
+    paragraph.innerText = quote;
+    document.getElementById("list-"+listName).appendChild(paragraph);
+}
+
+setInterval(getKanyeQuote, 10000);
