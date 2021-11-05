1017f4e474118e6bb8edaffe85acac029d91a1a2
Anders Latif <andl@kea.dk>
2021-11-05 03:23:32 +0100

    Recommited from last attempt

--- 9._Galleries_Frontend/galleries/galleries.js
+++ 9._Galleries_Frontend/galleries/galleries.js
@@ -1,18 +1,36 @@
-const galleriesWrapperElement = document.getElementById("galleries-wrapper");
+const galleriesTableBody = document.getElementById("galleries-tbody");
 
 fetch(baseURL + "/galleries")
 .then(response => response.json())
 .then(galleries => {
     galleries.map(gallery => {
-        const galleryElement = document.createElement("div");
+        const galleryTableRow = document.createElement("tr");
 
-        galleryElement.innerHTML = `
-            <a href="./gallery.html?galleryId=${gallery.id}">
-                <p>${escapeHTML(gallery.name)}</p>
-            </a>
+        galleryTableRow.innerHTML = `
+            <td>
+                <a href="./gallery.html?galleryId=${gallery.id}">
+                    <p>${escapeHTML(gallery.name)}</p>
+                </a>
+            </td>
+            <td>
+                <p>${escapeHTML(gallery.location)}</p>
+            </td>
+            <td>
+                <p>${escapeHTML(gallery.owner)}</p>
+            </td>
+            <td>
+                <p>${escapeHTML(gallery.squareFeet.toString())}</p>
+            </td>           
+            <td>
+                <button onclick="deleteGallery(${gallery.id})">❌</button>            
+            </td>
         `;
 
-        galleriesWrapperElement.appendChild(galleryElement);
+        galleriesTableBody.appendChild(galleryTableRow);
     });
+
 });
 
+function deleteGallery(galleryId) {
+    console.log(galleryId);
+}
